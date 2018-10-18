e0e60ce4f60b95ce389ee7f5afd8f6a0512ea60f
Tibet Teixeira <tibetbrasil@gmail.com>
2018-10-18 13:59:04 -0300

    Criação da view

---
+++ SpotPer/src/view/Gravadora.java
@@ -0,0 +1,54 @@
+package view;
+
+/**
+ *
+ * @author Tibet Teixeira
+ */
+public class Gravadora {
+    
+    int codigoGravadora;
+    String homePage;
+    String rua;
+    String cidade;
+    String pais;
+
+    public int getCodigoGravadora() {
+        return codigoGravadora;
+    }
+
+    public void setCodigoGravadora(int codigoGravadora) {
+        this.codigoGravadora = codigoGravadora;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public void setHomePage(String homePage) {
+        this.homePage = homePage;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
