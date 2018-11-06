2d8ee5238353f30ad8c8f70347decae4c932b8f8
Tibet Teixeira <tibetbrasil@gmail.com>
2018-11-06 13:02:43 -0300

    Package Model

---
+++ SpotPer/src/model/TelefoneGravadora.java
@@ -0,0 +1,26 @@
+package Model;
+
+/**
+ *
+ * @author Tibet Teixeira
+ */
+public class TelefoneGravadora {
+    int codigoGravadora;
+    String telefone;
+
+    public int getCodigoGravadora() {
+        return codigoGravadora;
+    }
+
+    public void setCodigoGravadora(int codigoGravadora) {
+        this.codigoGravadora = codigoGravadora;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }    
+}
