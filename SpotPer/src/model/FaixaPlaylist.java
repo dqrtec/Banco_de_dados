989dbaac74626138227e9688caa63436eb21d6b0
Tibet Teixeira <tibetbrasil@gmail.com>
2018-11-07 14:21:49 -0200

    Adicionado alguns aributos de faixa

--- SpotPer/src/model/FaixaPlaylist.java
+++ SpotPer/src/model/FaixaPlaylist.java
@@ -4,10 +4,28 @@ package Model;
  *
  * @author Tibet Teixeira
  */
-public class PlaylistFaixa {
+public class FaixaPlaylist {
     int idPlaylist;
     int numFaixa;
     int idAlbum;
+    int quantidadeTocada;
+    String dataUltimaVezTocada;
+
+    public int getQuantidadeTocada() {
+        return quantidadeTocada;
+    }
+
+    public void setQuantidadeTocada(int quantidadeTocada) {
+        this.quantidadeTocada = quantidadeTocada;
+    }
+
+    public String getDataUltimaVezTocada() {
+        return dataUltimaVezTocada;
+    }
+
+    public void setDataUltimaVezTocada(String dataUltimaVezTocada) {
+        this.dataUltimaVezTocada = dataUltimaVezTocada;
+    }
 
     public int getIdPlaylist() {
         return idPlaylist;
