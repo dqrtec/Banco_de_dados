ceca64b544b67431d9e780909e17939654348ac6
dqrtec <dqrtec@hotmail.com>
2018-11-08 17:51:57 -0200

    Insert Teste do Spotper

---
+++ SpotPer/insert_teste.sql
@@ -0,0 +1,7 @@
+insert into gravadora values (222, 'Sony', NULL, 'Coreia', 'Seul', 'Seila')
+
+insert into album values (111, 'Album 1', 'cartao', '2018-02-22', '2018-02-22', 25.5, 222)
+
+insert into tipo_composicao values (33, 'Rockzera')
+
+insert into faixa values (6, 111, 'Faixa 6', 2.50, 'cdd', 33)
