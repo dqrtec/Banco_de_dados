2e53e2f40d79f284f602fc1e2d4426a94b844a44
Tibet Teixeira <tibetbrasil@gmail.com>
2018-11-19 16:20:19 -0200

    Tuplas teste

--- SpotPer/banco/insert_teste.sql
+++ SpotPer/banco/insert_teste.sql
@@ -1,7 +1,12 @@
 insert into gravadora values (222, 'Sony', NULL, 'Coreia', 'Seul', 'Seila')
 
 insert into album values (111, 'Album 1', 'cartao', '2018-02-22', '2018-02-22', 25.5, 222)
+insert into album values (222, 'Album 2', 'cartao', '2018-02-23', '2018-02-23', 35.5, 222)
 
-insert into tipo_composicao values (33, 'Rockzera')
+insert into tipo_composicao values (33, 'Rock')
+insert into tipo_composicao values (22, 'MPB')
 
-insert into faixa values (6, 111, 'Faixa 6', 2.50, 'cdd', 33)
+insert into faixa values (1, 111, 'Faixa 1', 1.50, 'cdd', 33)
+insert into faixa values (2, 111, 'Faixa 2', 2.50, 'cdd', 22)
+insert into faixa values (3, 222, 'Faixa 3', 3.50, 'cdd', 33)
+insert into faixa values (4, 222, 'Faixa 4', 4.50, 'cdd', 22)
