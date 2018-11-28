60459faade2111b871802ba6946ee24c7a180c25
danieç <edaybrasil@gmail.com>
2018-11-28 15:13:38 -0200

    conserto pequenos erros

--- SpotPer/banco/bancoSpotper.sql
+++ SpotPer/banco/bancoSpotper.sql
@@ -291,11 +291,11 @@ GO
 
 -- 9 b)
 -- 9 c)
-select top 1 c.nome, count(*) from compositor c, compositor_faixa cf, faixa_playlist fp
+select top 1 c.nome, count(*) from compositor c, faixa_compositor cf, faixa_playlist fp
 where 
 	c.id_compositor = cf.id_compositor and
 	cf.num_faixa = fp.num_faixa and
-	cf.id_album = fp.id_album and
+	cf.id_album = fp.id_album
 group by c.id_compositor, c.nome
 GO
 -- 9 d)
