5750838bf46590487347cf461044d4de6e97249a
danieç <dqrtec@hotmail.com>
2018-11-27 23:42:57 -0200

    desnecessario 9c

--- SpotPer/banco/9c.sql
+++
@@ -1,6 +0,0 @@
-select top 1 c.nome, count(*) from compositor c, compositor_faixa cf, faixa_playlist fp
-where 
-	c.id_compositor = cf.id_compositor and
-	cf.num_faixa = fp.num_faixa and
-	cf.id_album = fp.id_album and
-group by c.id_compositor, c.nome
